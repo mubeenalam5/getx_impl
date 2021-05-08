01c9d5ecf1c4d815fd33677ac4a9cccd0a46e0e3
Mubeen Alam Usmani <57857826+mubeenalam5@users.noreply.github.com>
2021-05-08 13:46:35 +0500

    initial commit

---
+++ lib/views/products.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_impl/controllers/cart.dart';
+import 'package:getx_impl/controllers/shopping.dart';
+
+class ShoppingScr extends StatelessWidget {
+  final shoppingController = Get.put(ShoppingController());
+  final cartController = Get.put(CartController());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: GetX<ShoppingController>(
+                builder: (controller) {
+                  return Scrollbar(
+                    child: ListView.builder(
+                      itemCount: controller.products.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          margin: const EdgeInsets.all(12),
+                          color: Colors.blue,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          '${controller.products[index].productName}',
+                                          style: TextStyle(fontSize: 24),
+                                        ),
+                                        Text(
+                                            '${controller.products[index].productDescription}'),
+                                      ],
+                                    ),
+                                    Text(
+                                        '\$${controller.products[index].price}',
+                                        style: TextStyle(fontSize: 24)),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    RaisedButton(
+                                      onPressed: () {
+                                        cartController.addToCart(
+                                            controller.products[index]);
+                                      },
+                                      color: Colors.blue,
+                                      textColor: Colors.white,
+                                      child: Text('Add to Cart'),
+                                    ),
+                                    RaisedButton(
+                                      onPressed: () {
+                                        cartController.removeFromCart(
+                                            controller.products[index]);
+                                      },
+                                      color: Colors.blue,
+                                      textColor: Colors.white,
+                                      child: Text('Remove from  Cart'),
+                                    ),
+                                  ],
+                                ),
+                                Obx(() => IconButton(
+                                      icon: controller
+                                              .products[index].isFavorite.value
+                                          ? Icon(Icons.check_box_rounded)
+                                          : Icon(Icons
+                                              .check_box_outline_blank_outlined),
+                                      onPressed: () {
+                                        controller.products[index].isFavorite
+                                            .toggle();
+                                      },
+                                    ))
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+            GetX<CartController>(builder: (controller) {
+              return Text(
+                'Total amount: \$ ${controller.totalPrice}',
+                style: TextStyle(fontSize: 32, color: Colors.white),
+              );
+            }),
+            SizedBox(height: 100),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        backgroundColor: Colors.amber,
+        icon: Icon(
+          Icons.add_shopping_cart_rounded,
+          color: Colors.black,
+        ),
+        label: GetX<CartController>(builder: (controller) {
+          return Text(
+            controller.count.toString(),
+            style: TextStyle(color: Colors.black, fontSize: 24),
+          );
+        }),
+      ),
+    );
+  }
+}
